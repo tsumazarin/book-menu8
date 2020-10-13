b8db2e861e0dea6814b3283c5e19db959a61d893
tsumazarin <69566581+tsumazarin@users.noreply.github.com>
2020-10-13 11:43:18 +0900

    flysystem

--- app/Http/Controllers/Product/ProductAddController.php
+++ app/Http/Controllers/Product/ProductAddController.php
@@ -30,7 +30,7 @@ class ProductAddController extends Controller
         $product_title = $request->title;
         $product_price = $request->price;
         $product_image = $request->image;
-
+        dd($product_image);
         //画像をアップロード
         $read_path = Storage::disk('s3')->putFile('/book-menu8', $product_image, 'public');
 
